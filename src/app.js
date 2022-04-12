6b629f5f837dd477840d096532a3ae7f59478516
kruti <krutikakamli@gmail.com>
2022-04-12 17:26:50 +0530

    updated in app.js

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ app.get("/weather" , (req, res) =>
 app.get("/*" , (req, res) =>
 {
     res.render('404error', {
-        errorMsg : "OOps! Page not found..",
+        errorMsg : "OOps! Page not found, plz go back to home page",
     } );
 })
 
